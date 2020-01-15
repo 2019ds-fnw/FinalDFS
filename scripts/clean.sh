709cffac6d49270bf3533e1ea3d718cb4d9fa9a3
libre <branchvan379@gmail.com>
2020-01-15 19:28:33 +0800

    add: clean docker network

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,3 +2,4 @@
 
 sudo docker container rm -f $(sudo docker container ls -aqf name=minion)
 sudo docker container rm -f $(sudo docker container ls -aqf name=master)
+sudo docker network rm dfs-network
